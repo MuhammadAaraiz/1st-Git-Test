b0859e42a1a1ccc4e500c06c20bfc62b1bebffb3
muhammadaaraiz619@gmail.com <muhammadaaraiz619@gmail.com>
2023-09-05 14:34:34 -0700

    Work Complete

---
+++ app.js
@@ -0,0 +1,132 @@
+// Chapter 1
+// Q1
+// var firstName="Muhammad \n"
+
+// var lastName="Aaraiz\n"
+
+// var email="abc123gmailcom\n"
+
+// var number="XXX-1234567-X\n"
+
+// var password="abc1234"
+
+// console.log(firstName+lastName+email+number+password)
+
+// alert(firstName+lastName+email+number+password);
+
+// // Q2
+// console.log("You're learning JavaScript!");
+
+// alert("Hello, World!");
+
+
+// Chapter 2
+// Q1
+// var VariableName = "CamelVariable";
+
+// // Q2
+// var myVariable; // Declaration without definition
+
+// myVariable = "This is a string value"; // Assigning a string value
+
+// // Q3
+// var teamName = "Knight Riders";
+// alert("My team name is: " + teamName);
+
+// //Q4
+// var bestMan = "John";
+
+
+
+// Chapter 3
+// Q1
+// var caseQty;
+
+
+// // Q2
+// caseQty = 144;
+
+// // Q3
+// var num = parseInt("9");
+
+// // Q4
+// var num1, num2;
+// num1 = 3;
+// num2 = 5;
+// var sum = num1 + num2; // sum now contains the result of 3 + 5, which is 12
+
+
+
+// //  Q5
+
+// var TotalPlayers = 30;
+// var Totalkits = 10;
+// var orderTotal = TotalPlayers + Totalkits;
+
+
+
+
+// //Q6
+
+// var firstNumber = 5;
+// var secondNumber = firstNumber + 10;
+
+
+
+// Chapter 4
+// Q1
+// var productCost = 3.45;
+
+// // Q2
+// var nameOfTeam;
+
+// // Q3
+// var myNumber = 21;
+
+// // Q4
+// var firstName = "Muhammad";
+// var lastName = "Aaraiz";
+// var fullName = firstName + lastName;
+
+// // Q5
+
+// var LegalVariableNames="LegalVariableNames:<br><br> myVariable <br>_privateVariable <br>$specialVariable <br> num123 <br> camelCaseVariable"
+// document.write (LegalVariableNames)
+
+// var IllegalVariableNames="<br><br>IllegalVariableNames:<br> <br>123illegalVariable  (Cannot start with a number) <br> illegal-variable (Cannot use hyphens)<br>illegal variable (Cannot contain spaces) <br>function (Reserved keyword) <br>for (Reserved keyword)  "
+
+// document.write (IllegalVariableNames)
+
+
+
+// Chapter 5
+// Q1
+// var air="The arithmetic operator that gives you the remainder when one number is divided by another is called the modulo operator. In most programming languages, including JavaScript, the symbol for the modulo operator is %";
+
+
+// // Q2
+// var  num="The value of `num` in the expression `var num = 20 % 6;` is `2`. The `%` operator calculates the remainder when the number on the left in this case, 20 is divided by the number on the right in this case, 6. So, 20 divided by 6 equals 3 with a remainder of 2, and that remainder is assigned to the variable `num`."
+
+
+// // Q3
+// var largeNum = 1000 * 2000;
+
+
+// // Q4
+// var variable1 = 20;
+// var variable2 = 10;
+// var result = variable1 - variable2;
+
+
+// // Q5
+// var remainderValue = 15 % 7;
+
+// // Q6
+// var num1 = 10;
+// var num2 = 20;
+// var result = num1 * num2;
+
+// alert("The result of multiplying " + num1 + " and " + num2 + " is: " + result);
+
+
+
